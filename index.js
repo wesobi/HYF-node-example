6e4b8e9cc1ed8c45c27c573bde1a22c4e0e801fa
Wesley Vanbrabant <wesley.vanbrabant@showpad.com>
2019-01-07 23:37:29 +0100

    small switch for routing explanation

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
 const logColor = require("./log-color");
+const http = require("http");
 
-logColor.red("string");
-logColor.green("string");
-logColor.blue("string");
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  const url = req.url;
+
+  switch (url) {
+    case "/red":
+      logColor.red("red string");
+      res.end("red string sent to console");
+      break;
+
+    case "/blue":
+      logColor.blue("blue string");
+      res.end("blue string sent to console");
+      break;
+
+    case "/green":
+      logColor.green("green string");
+      res.end("green string sent to console");
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.end("page not found");
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at ${port}`);
+});
